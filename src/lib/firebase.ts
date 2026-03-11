e4405fb496bd4cdfe59e3e135e4d262ab4a8878f
Schulz <schulz@example.com>
2026-03-11 11:50:37 -0300

    Update firebase.ts: Clear config values

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,14 +2,15 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAUbjCu6QkG2izFM2lH037m5LvYQ3qZ-iw",
-  authDomain: "expensetracker-f6ebe.firebaseapp.com",
-  projectId: "expensetracker-f6ebe",
-  storageBucket: "expensetracker-f6ebe.firebasestorage.app",
-  messagingSenderId: "362690546953",
-  appId: "1:362690546953:web:ccc3b50e11b58920fd17d8",
-  measurementId: "G-NZ0SPSVMEQ"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
+
 
 // Initialize Firebase
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
